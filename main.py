137b8a30e9bffab85378bbe5da0474de4474e527
Dohyun Kim <00dh.kim@gmail.com>
2021-05-14 03:10:14 +0900

    .

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ class Turtlable:
         turtle.goto(init_x, init_y)
         turtle.down()
 
-    def turtle_draw(self, x, y):
-        turtle.goto(x, y)
+    # 바뀐 위치로 터틀 이동
+    def turtle_draw(self):
+        turtle.goto(self.pos.x, self.pos.y)
 
 
 class Missile(Turtlable):
@@ -33,6 +34,19 @@ class Missile(Turtlable):
         super().__init__(x, y)
         self.v_0 = 0 # 초기속도
         self.theta = 0 # 초기 발사각, 라디안 단위
+
+    def calc_pos(self, t):
+        x = pos_x_over_time(self.v_0, self.theta, t)
+        y = pos_y_over_time(self.v_0, self.theta, t)
+
+        self.pos.x = x
+        self.pos.y = y
+
+
+
+class AttackMissile(Missile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
     def launch_by_distance(self, distance):
         # theta의 최대, 최소 범위, 기본값은 0과 1
@@ -49,7 +63,6 @@ class Missile(Turtlable):
 
         self.launch(v_0, theta)
 
-
     # 처음 1회만 실행
     def launch(self, v_0, theta):
         self.pos = Pos(self.init_pos.x, self.init_pos.y)
@@ -63,23 +76,21 @@ class Missile(Turtlable):
             self.calc_pos(time)
             print(f"time: {time} {self.pos}")
             time += 0.1
-
-            self.turtle_draw(self.pos.x, self.pos.y)
+            self.turtle_draw()
 
         turtle.up()
         turtle.goto(-10,-10)
 
-    def calc_pos(self, t):
-        x = pos_x_over_time(self.v_0, self.theta, t)
-        y = pos_y_over_time(self.v_0, self.theta, t)
+class DefenseMissile(Missile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
-        self.pos.x = x
-        self.pos.y = y
-
-
+    def launch_intercept(self):
+        # TODO: 시간 처리를 어떻게 할 것인가?
+        # TODO: 왼쪽으로 발사되는 수식을 계산하기
 
 if __name__ == '__main__':
-    m = Missile(0,0)
+    m = AttackMissile(0,0)
     m.pos.x = 10
     print(m.pos)
     #m.launch(50, math.pi/3)
