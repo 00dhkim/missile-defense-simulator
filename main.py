b9a35126904151f4c6b8fe0566a3d031e9413773
Dohyun Kim <00dh.kim@gmail.com>
2021-05-14 12:12:45 +0900

    방어미사일(요격미사일) 구현 완료, 수식 에러 수정 필요

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ from dataclasses import dataclass
 import math
 import random
 import turtle
+from abc import *
 
-def pos_x_over_time(v_0, theta, t):
-    return v_0 * math.cos(theta) * t
-
-def pos_y_over_time(v_0, theta, t):
-    return -0.5 * 9.8 * t * t + v_0 * math.sin(theta) * t
 
 @dataclass
 class Pos:
@@ -18,39 +14,61 @@ class Turtlable:
     def __init__(self, x, y):
         self.init_pos = Pos(x, y)
         self.pos = Pos(self.init_pos.x, self.init_pos.y)
+        self.turtle = turtle.Turtle()
 
     def turtle_init(self, init_x, init_y):
-        turtle.up()
-        turtle.goto(init_x, init_y)
-        turtle.down()
+        self.turtle.up()
+        self.turtle.goto(init_x, init_y)
+        self.turtle.down()
 
     # 바뀐 위치로 터틀 이동
     def turtle_draw(self):
-        turtle.goto(self.pos.x, self.pos.y)
+        self.turtle.goto(self.pos.x, self.pos.y)
 
 
 class Missile(Turtlable):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.v_0 = 0 # 초기속도
-        self.theta = 0 # 초기 발사각, 라디안 단위
+        self.v_0 = 0.0 # 초기속도
+        self.theta = 0.0 # 초기 발사각, 라디안 단위
+        self.time = 0.0
+        self.state = ''
 
     def calc_pos(self, t):
-        x = pos_x_over_time(self.v_0, self.theta, t)
-        y = pos_y_over_time(self.v_0, self.theta, t)
+        x = self.pos_x_over_time(self.v_0, self.theta, t)
+        y = self.pos_y_over_time(self.v_0, self.theta, t)
 
         self.pos.x = x
         self.pos.y = y
 
+    @abstractmethod
+    def pos_x_over_time(self, v_0, theta, t):
+        pass
+
+    @abstractmethod
+    def pos_y_over_time(self, v_0, theta, t):
+        pass
 
 
 class AttackMissile(Missile):
     def __init__(self, x, y):
         super().__init__(x, y)
+        self.turtle.pencolor('red')
+        # 가능한 state 종류
+        # standby: 발사 대기중
+        # flying: 미사일 날아가는 중
+        # impacted: 목표 지점에 착탄함
+        # intercepted: 중간에 요격됨
 
-    def launch_by_distance(self, distance):
+    def pos_x_over_time(self, v_0, theta, t):
+        return v_0 * math.cos(theta) * t
+
+    def pos_y_over_time(self, v_0, theta, t):
+        return -0.5 * 9.8 * t * t + v_0 * math.sin(theta) * t
+
+    def launch_init_by_distance(self, distance):
         # theta의 최대, 최소 범위, 기본값은 0과 1
-        lower = 0 # 0 이상 필요
+        lower = 0.7 # 0 이상 필요
         upper = 1 # 1 이하 필요
 
         theta = (upper - lower) * random.random() * math.pi / 2 + lower
@@ -61,44 +79,132 @@ class AttackMissile(Missile):
         # 이때의 착탄 시간: 2 * v_0 * math.sin(theta) / 9.8
         # 이때의 최대 높이: v_0 ** 2 * math.sin(theta) ** 2 / 2 / 9.8
 
-        self.launch(v_0, theta)
+        self.launch_init(v_0, theta)
 
     # 처음 1회만 실행
-    def launch(self, v_0, theta):
+    def launch_init(self, v_0, theta):
         self.pos = Pos(self.init_pos.x, self.init_pos.y)
         self.v_0 = v_0
         self.theta = theta
+        self.state = 'standby'
 
         self.turtle_init(0, 0)
-        time = 0.0
-        # 땅에 닿기 전까지
-        while self.pos.y >= 0:
-            self.calc_pos(time)
-            print(f"time: {time} {self.pos}")
-            time += 0.1
-            self.turtle_draw()
+        self.time = 0.0
 
-        turtle.up()
-        turtle.goto(-10,-10)
+    # 0.1초 진행마다 실행
+    def launching(self, time):
+        self.time = time
+        self.state = 'flying'
+        self.calc_pos(self.time)
+        print(f"[ATTACK ] {self.state} time: {self.time} {self.pos}")
+        self.turtle_draw()
+
+        if self.pos.y < 0:
+            self.state = 'impacted'
+
+    # launching 끝나고 한번 실행
+    def launch_end(self):
+        self.turtle.up()
+        self.turtle.goto(-10, -10)
+
+    def intercepted(self):
+        self.state = 'intercepted'
 
 class DefenseMissile(Missile):
     def __init__(self, x, y):
         super().__init__(x, y)
+        self.turtle.pencolor('blue')
+        self.turtle.up()
+        self.turtle.goto(x,0)
+        self.turtle.down()
+        # 가능한 state 종류
+        # standby: 발사 대기중
+        # flying: 미사일 날아가는 중 (목표 지점 이전)
+        # intercept_success: 요격 성공
+        # intercept_passed: 요격 실패 후 지나침
+        # impacted: 땅에 착탄
 
-    def launch_intercept(self):
-        # TODO: 시간 처리를 어떻게 할 것인가?
-        # TODO: 왼쪽으로 발사되는 수식을 계산하기
+    def pos_x_over_time(self, v_0, theta, t):
+        return self.init_pos.x - v_0 * math.cos(theta) * t
+
+    def pos_y_over_time(self, v_0, theta, t):
+        return -0.5 * 9.8 * t * t + v_0 * math.sin(theta) * t
+
+    def launch_init(self, v_a, theta_a):
+        # 공격미사일의 초기속도와 초기발사각을 받아옴
+        self.pos = Pos(self.init_pos.x, self.init_pos.y)
+        self.theta = math.atan(9.8 / 2 / v_a / math.cos(theta_a))
+        self.v_0 = v_a * math.cos(theta_a) / math.cos(self.theta)
+        self.state = 'standby'
+
+    def launching(self, time, a_pos_x, a_pos_y):
+        self.time = time
+        self.state = 'flying'
+        self.calc_pos(self.time)
+        print(f"[DEFENSE] {self.state} time: {self.time} {self.pos}")
+        self.turtle_draw()
+
+        if self.pos.y < 0:
+            self.state = 'impacted'
+        elif (self.pos.x - a_pos_x)**2 + (self.pos.y - a_pos_y)**2 < 1:
+            self.state = 'intercept_success'
+        elif self.pos.x < a_pos_x:
+            self.state = 'intercept_passed'
+
+    # 땅에 닿을 경우와 요격한 경우 각각 따로
+    def launch_end(self):
+        self.turtle.up()
+        self.turtle.goto(-20, -20)
+
 
 if __name__ == '__main__':
-    m = AttackMissile(0,0)
-    m.pos.x = 10
-    print(m.pos)
-    #m.launch(50, math.pi/3)
-    m.launch_by_distance(100)
-    m.launch_by_distance(100)
-    m.launch_by_distance(100)
-    m.launch_by_distance(100)
-    m.launch_by_distance(100)
+    am = AttackMissile(0,0)
+    dm = DefenseMissile(40, 0)
+    time = 0.0
+
+    am.launch_init_by_distance(100)
+    dm.launch_init(am.v_0, am.theta)
+
+    # ------------------------------------------------------
+    # 공격미사일과 방어미사일이 동시에 날아가는 시뮬레이션
+    '''
+    while (am.state == 'standby' or am.state == 'flying') and \
+            (dm.state == 'standby' or dm.state == 'flying'):
+
+        am.launching(time)
+        if time >= 1.0:
+            dm.launching(time, am.pos.x, am.pos.y)
+
+        time += 0.1
+
+        input()
+
+    am.launch_end()
+    dm.launch_end()
+    '''
+    # ------------------------------------------------------
+
+    # ------------------------------------------------------
+    # 공격미사일 먼저, 그 후 방어미사일 각각 날아가는 시뮬레이션
+    '''
+    while am.state == 'standby' or am.state == 'flying':
+        am.launching(time)
+        time += 0.1
+    am.launch_end()
+
+    time = 0.0
+    while dm.state != 'impacted':
+        dm.launching(time, am.pos.x, am.pos.y)
+        time += 0.1
+    dm.launch_end()
+    
+    '''
+    # ------------------------------------------------------
+
+
+    print('am:', am.state, 'dm:', dm.state)
+
+
 
     input("Press any key")
 
